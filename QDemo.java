b250c102651313e6203e61f8dbcda3c47cd027c5
alexander <alklas@mail.ru>
2025-11-11 12:56:12 +0700

    Добавили два новых конструктора в класс Queue и продемонстрировали их работу

--- QDemo.java
+++ QDemo.java
@@ -2,9 +2,30 @@ class Queue {
 	private char q[]; //Ссылка на массив, хранящий элементы очереди
 	private int putloc, getloc; //Указатели на начало и на конец очереди
 	
+	//Конструктор для создания пустой очереди
 	Queue(int size) {
 		q = new char[size+1]; //Выделение памяти для массива
 		putloc = getloc = 0;
+	}
+
+	//Конструктор для копирования существующей очереди в новую очередь
+	Queue(Queue ob) {
+		putloc = ob.putloc;
+		getloc = ob.getloc;
+		q = new char[ob.q.length];
+
+		for(int i = getloc+1; i <= putloc; i++)
+			q[i] = ob.q[i];
+	}
+	
+	//Конструктор для помещения значений из массива в новую очередь
+	Queue(char a[]) {
+		putloc = 0;
+		getloc = 0;
+		q = new char[a.length+1];
+
+		for(int i = 0; i < a.length; i++)
+			put(a[i]);
 	}
 
 	//Метод для помещения символа в очередь
@@ -66,11 +87,11 @@ class QDemo {
 			if(ch != (char) 0)
 				System.out.print(ch);
 
-		System.out.println("Попытка вставки символов в пустую очередь smallQ: ");
+		System.out.println(" Попытка извлечения символа из очереди smallQ: ");
 		}
 
 		for(i = 0; i < 5; i++) {
-			System.out.print("Попытка сохранения " + (char) ('Я' - i));
+			System.out.print(" Попытка сохранения " + (char) ('Я' - i));
 
 			smallQ.put((char) ('Я' - i));
 			System.out.println();
@@ -80,7 +101,46 @@ class QDemo {
 		//test.q[0] =  'A';
 		//test.putloc = 'B';
 		test.put('A');
-		System.out.println("Извлечение символа из очереди test" + test.get());
+		System.out.println("Извлечение символа из очереди test " + test.get());
 		}
+
+		//Создание пустой очереди
+		Queue q1 = new Queue(10);
+
+		char arr1[] = {'Q', 'W', 'E', 'R', 'T', 'Y'};
+		//Создание очереди на основе массива arr1[]
+		Queue q2 = new Queue(arr1);
+
+		//Помещение симоволов в очередь q1
+		for(i = 0; i < 10; i++)
+			q1.put((char) ('Y' + i));
+
+		//Создание очереди q3 на основе очереди q1
+		Queue q3 = new Queue(q1);
+
+		//Отображаем содержимое очередей q1, q2, q3
+		
+		System.out.println();
+
+		System.out.print("Очередь q1: ");
+		for(i = 0; i < 10; i++) {
+			ch = q1.get();
+			System.out.print(ch);
+		}
+		System.out.println("\n");
+		
+		System.out.print("Очередь q2: ");
+		for(i = 0; i < 10; i++) {
+			ch = q2.get();
+			System.out.print(ch);
+		}
+		System.out.println();
+
+		System.out.print("Очередь q3: ");
+		for(i = 0; i < 10; i++) {
+			ch = q3.get();
+			System.out.print(ch);
+		}
+		System.out.println();
 	}
 }
