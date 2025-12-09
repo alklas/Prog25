b4947474e375b5bdceaaa65349ac5434b75d982a
alklas <alklas@mail.ru>
2025-12-09 15:39:19 +0700

    Работа с пакетами, реализация интерфейсов

---
+++ ICharQ.java
@@ -0,0 +1,4 @@
+public interface ICharQ {
+	void put(char ch); //Помещение символа в очередь
+	char get(); //Извлечение символа из очереди
+}
---
+++ Pr_111.java
@@ -0,0 +1,9 @@
+//import java.util.*;
+
+class Pr111 {
+	public static void main(String[] args) {
+		List <String> list = new ArrayList<> ();
+		list.add("Строка 1");
+		list.add("Строка 2");
+	}
+}	
